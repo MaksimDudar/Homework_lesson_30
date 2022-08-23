c93dfe872de0bc75101aa4aaa129fb16a1592caa
MaksimDudar <maksimdudar1@gmail.com>
2022-08-24 01:14:31 +0300

    somithing work (spend 4 hours )))) )

--- app.rb
+++ app.rb
@@ -8,10 +8,12 @@ set :database, {adapter: "sqlite3", database: "homework.db"}
 
 class Post < ActiveRecord::Base
 end
-
+class Comment < ActiveRecord::Base
+end
 
 get '/' do
 	@posts = Post.order('created_at DESC')
+	@comments = Comment.order('created_at DESC')
 	erb :index
 end
 
@@ -30,3 +32,19 @@ post '/new' do
 		erb :new
 	end
 	end
+
+get '/post/:post' do
+	 @c = Comment.new
+		erb :post
+	end
+
+
+post '/comment' do
+			@c = Comment.new params[:comment]
+			if @c.save
+				redirect to '/'
+			else
+				@error = @p.errors.full_messages.first
+				erb :post
+	end
+end
